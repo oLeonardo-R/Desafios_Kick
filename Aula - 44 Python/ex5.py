6711aabe3c5204304b488b89aeccd83e135c1ce9
Seu Nome <leo.rocha1@hotmail.com>
2024-08-12 20:23:38 -0300

    att

--- Aula - 44 Python/ex5.py
+++ Aula - 44 Python/ex5.py
@@ -1,9 +1,5 @@
-i = 1
-while i <= 10:
-    print(f"Tabuada do {i}:")
-    j = 1
-    while j <= 10:
-        print(f"{i} x {j} = {i * j}")
-        j += 1
-    print()  
-    i += 1
+numero = int(input("Digite um número para ver a sua tabuada: "))
+
+print(f"Tabuada do {numero}:")
+for i in range(1, 11):
+    print(f"{numero} x {i} = {numero * i}")
